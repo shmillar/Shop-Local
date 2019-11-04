6da9c1ec46178b72b7fe53b80a75240c8a355411
Sam Millar <shmillar@asu.edu>
2019-11-03 17:33:05 -0700

    Initial Commit

---
+++ shopLocal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopLocal
+//
+//  Created by Sam Millar on 11/3/19.
+//  Copyright © 2019 Sam Millar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
